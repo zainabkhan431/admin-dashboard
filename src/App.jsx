c70215c104eecfb1f088534e81fe0894abfdba94
zainabkhan431 <104517696+zainabkhan431@users.noreply.github.com>
2025-03-08 20:40:05 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import React from "react"
+import { Route, Routes } from "react-router-dom"
+import OverviewPage from "./pages/OverviewPage";
+import ProductPage from "./pages/ProductPage";
+
+function App() {
+
+  return (
+    <div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+
+<div className='fixed inset-0 -z-10'>
+				<div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80' />
+				<div className='absolute inset-0 backdrop-blur-sm' />
+			</div>
+<Routes>
+  <Route path="/" element={<OverviewPage/>}></Route>
+  <Route path="/Productpage" element={<ProductPage/>}></Route>
+
+</Routes>
+</div>
+  )
+}
+
+export default App
---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OverviewPage = () => {
+  return (
+    <div>
+      Overview Page
+    </div>
+  )
+}
+
+export default OverviewPage
