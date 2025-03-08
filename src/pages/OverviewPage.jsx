6b5d4184f8bdd023a8ebf74aa8d36d704a1f5f8e
zainabkhan431 <104517696+zainabkhan431@users.noreply.github.com>
2025-03-08 22:55:06 +0500

    overview page completed

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -3,6 +3,10 @@
 import React from 'react'
 import Header from '../Components/Common/Header';
 import StatCard from "../Components/Common/StatCard"
+import SalesOverview from "../Components/overview/SalesOverview";
+import CategoryDistributionChart from "../Components/overview/CategoryDistributionChart";
+import SalesChannelChart from "../Components/overview/SalesChannelChart";
+
 import { motion } from 'framer-motion';
 import { BarChart2, ShoppingBag, Users, Zap } from 'lucide-react'
 const OverviewPage = () => {
@@ -10,7 +14,6 @@ const OverviewPage = () => {
     <div className='flex-1 overflow-auto relative z-10'>
       <Header title={'Overview'}/>
       <main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
-				{/* STATS */}
 				<motion.div
 					className='grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8'
 					initial={{ opacity: 0, y: 20 }}
@@ -22,6 +25,12 @@ const OverviewPage = () => {
 					<StatCard name='Total Products' icon={ShoppingBag} value='567' color='#EC4899' />
 					<StatCard name='Conversion Rate' icon={BarChart2} value='12.5%' color='#10B981' />
 				</motion.div>
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
+					<SalesOverview/>
+                    <CategoryDistributionChart />
+					<SalesChannelChart />
+					
+				</div>
                 </main>
     </div>
   )
